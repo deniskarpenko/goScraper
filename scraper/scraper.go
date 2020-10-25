61d0f6755072958cafea363af9f31fe433f061ea
Denis Karpenko <new.den84@gmail.com>
2020-10-25 13:10:54 +0300

    fix

---
+++ scraper/scraper.go
@@ -0,0 +1,17 @@
+package scraper
+
+import (
+	"fmt"
+)
+
+type Scraper struct {
+	url string
+}
+
+func (s *Scraper) GetUrl() {
+	fmt.Println(s.url)
+}
+
+func (s *Scraper) SetUrl(url string) {
+	s.url = url
+}
